74defb572db43f2487fb5af2c301f1431ce140a8
Leksa <surnacheva@ithub.ru>
2021-11-30 13:04:54 +0300

    получение товаров из csv-файла

--- routes/server.js
+++ routes/server.js
@@ -1,8 +1,24 @@
 const router = require("express").Router();
+const fs = require("fs");
+
+let goodsNames = ["Название", "Категория", "Артикул", "Цвет", "Размер", "Кол-во", "Цена", "Бренд"];
+let fileText = fs.readFileSync("./data/goods.csv", "utf-8");
+// console.log(fileText);
+// let goods = fileText.split("\n"); - для UNIX!
+let goods = fileText.split("\r\n"); // - для Window
+
+for (let i = 0; i < goods.length; i++) {
+    //'Футболка;Одежда;00123;cyan;M;10;1000;ВсеМайки.ru', => ['Футболка','Одежда','00123','cyan','M','10','1000','ВсеМайки.ru']
+    goods[i] = goods[i].split(";");
+}
+// console.log(goods);
 
 router.get("/", function (req, res) {
     // res.send("<h1>Магазин одежды</h1>");
-    res.render("index");
+    res.render("index", {
+        names: goodsNames,
+        goods: goods
+    });
 });
 
 module.exports = router;
